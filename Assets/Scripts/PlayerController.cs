856456c797214a232e6a6979039780c381fb8f22
Andrew Huang <ahuang@sdccd.edu>
2021-02-09 00:05:19 -0800

    Unit 1: Lesson D

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 15.0f;
+    [Header("Input Sensitivity")]
+    public float accelerateSpeed = 15.0f;
+    public float turnSpeed = 30.0f;
+
+    private float horizontalInput;
+    private float verticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +20,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
         Debug.Log(Time.deltaTime);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // handle accelerator
+        transform.Translate(Vector3.forward * Time.deltaTime * accelerateSpeed * verticalInput);
+
+        // handle turning about Y-axis or Vector3.up
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
